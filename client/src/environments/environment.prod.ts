ca8ce6cb284c42d99a90085fc8ced08741be1434
Marek Kardos <marek.kardos@vitabook.de>
2021-01-09 22:42:52 +0100

    client: api path update

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'api/',
+  apiUrl: 'https://ecommnetapi.azurewebsites.net/api/',
   stripeSettings: {
     PublishableKey: 'pk_test_51I5A6QL9LzbX0TGfv5CuYr7xuET3rORDb3jbHIFD2QfSYK2le5EyhhMavjVvJx3DM12eg2b1RR8DjmfM4nw6Mcyl00jRYJIO8a'
   }
